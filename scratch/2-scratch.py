3f80b61d49932a38fc84db32b2ca857c03d9ef05
Peter <pech6593@colorado.edu>
2022-06-06 21:51:10 -0600

    figured it out! Didn't realize needed to match dimension along the Brillouin Zone

--- scratch/2-scratch.py
+++ scratch/2-scratch.py
@@ -1,3 +1,5 @@
+"""Calculate the dispersion of a 2D LiNbO3 waveguide"""
+
 import meep as mp
 import meep.materials as mt
 import matplotlib.pyplot as plt
@@ -16,8 +18,8 @@ w_wvgd = 0.5 * wl_wvgd / n_center_wvl  # width of the waveguide is half a wavele
 dpml = 1  # PML thickness
 
 center_wvgd = mp.Vector3(0, 0, 0)  # where the waveguide is centered
-sx = 6  # size of the cell in the x direction
 sy = 6  # size of the cell in y direction
+sx = 1 / (2 * mt.LiNbO3.valid_freq_range[-1])  # dimension along periodic direction needs to match the BZ!
 
 # %% create the geometric objects from the above
 blk = mp.Block(size=mp.Vector3(mp.inf, w_wvgd, mp.inf),
@@ -37,13 +39,15 @@ boundary_layers = [PML]
 Sources = []
 
 # %% create a gaussian source instance and place it at the front of the waveguide
-wl_src = 1.5
-df_src = 1.5
-src = mp.GaussianSource(wavelength=wl_src,
+bw = blk.material.valid_freq_range
+f_src = float((np.diff(bw) / 2) + bw[0])
+df_src = float(np.diff(bw) * 1)
+
+src = mp.GaussianSource(frequency=f_src,
                         fwidth=df_src
                         )
-pt_src = center_wvgd + mp.Vector3(0.0, 0.0 * w_wvgd)
-
+pt_src_offset = mp.Vector3(0, w_wvgd * 0.0)
+pt_src = center_wvgd + pt_src_offset
 source = mp.Source(
     src=src,
     component=mp.Ez,
@@ -57,57 +61,53 @@ sim = mp.Simulation(cell_size=cell,
                     geometry=geometry,
                     sources=Sources,
                     boundary_layers=boundary_layers,
-                    resolution=35,
+                    resolution=50,
                     )
 sim.use_output_directory('sim_output')
 
 # %% Exploit symmetries (if there are any)
-symm1 = mp.Symmetry(
+symy = mp.Symmetry(
     direction=mp.Y,
     phase=1
 )
-symm2 = mp.Symmetry(
+symx = mp.Symmetry(
     direction=mp.X,
     phase=1
 )
 
 sim.symmetries = [
-    symm1,
-    symm2
+    symy,
+    symx
 ]
 
-# %%
-# run to visualize the fields
-# sim.run(
-#     mp.to_appended("ez", mp.at_every(0.6, mp.output_efield_z)),
-#     # mp.at_beginning(mp.output_epsilon()),
-#     # until_after_sources=mp.stop_when_fields_decayed(50, mp.Ez, mon_pt, 1e-3),
-#     until_after_sources=300,
-# )
-
-# set periodic boundary conditions for a given k_point, run the sim, then anlayze result with Harmin repeatedly
+# %% Calculate Waveguide Dispersion
+# set periodic boundary conditions for a given k_point,
+# run the sim, then anlayze result with Harminv repeatedly
 # at multile k_points
 kmin, kmax = blk.material.valid_freq_range
-kpts = mp.interpolate(5, [mp.Vector3(kmin), mp.Vector3(kmax)])
+kpts = mp.interpolate(19, [mp.Vector3(kmin), mp.Vector3(kmax)])
 kx = np.array([i.x for i in kpts])
-freq = sim.run_k_points(500, kpts)
+freq = sim.run_k_points(300, kpts)
 
-# %%
 plt.figure()
-plt.plot([kx.min(), kx.max()], [kx.min(), kx.max()], 'k')
+plt.plot([kx.min(), kx.max()], [kx.min(), kx.max()], 'k', label='light line')
 for n in range(len(freq)):
     if len(freq[n]) > 0:
         [plt.plot(kx[n], i.real, marker='o', color='C0') for i in freq[n]]
+plt.legend(loc='best')
 
-# %% get data for gif / video
+# %% Visualize the Fields (Instead of the above Cell, Comment out one of them!)
+# sim.cell_size = mp.Vector3(sy, sy, 0)
+# sim.run(
+#     mp.to_appended("ez", mp.at_every(0.6, mp.output_efield_z)),
+#     until_after_sources=100,
+# )
+
+# %% animate the visualized fields
 # with h5py.File('sim_output/2-scratch-ez.h5', 'r') as f:
 #     data = np.array(f[util.get_key(f)])
 #
-# with h5py.File('sim_output/2-scratch-eps-000000.00.h5', 'r') as f:
-#     eps = np.array(f[util.get_key(f)])
-#
 # save = False
-#
 # fig, ax = plt.subplots(1, 1)
 # for n in range(0, data.shape[2], 1):
 #     ax.clear()
