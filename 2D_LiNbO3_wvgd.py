881ee0b76c065b6e7496357a4158616f20038250
Peter <pech6593@colorado.edu>
2022-06-09 20:49:53 -0600

    update

--- 2D_LiNbO3_wvgd.py
+++ 2D_LiNbO3_wvgd.py
@@ -1,4 +1,15 @@
-"""Getting started: 2D simulation in a straight LiNbO3 waveguide"""
+"""Getting started: 2D simulation in a straight LiNbO3 waveguide
+To calculate dispersion:
+    1. change wavelength range
+    2. sx = 0 -> automatic now
+    3. remove ABSX -> automatic now
+    4. remove mp.Mirror(direction=mp.X) -> automatic now
+    4. run "calculate dispersion" block instead of "visualize fields" block -> automatic now
+
+Parameters to play with
+    1. add a silica substrate -> set slc_sbstrt to True
+    2. change the wavelength range that sets the bandwidth of the source (wl_min, wl_max)
+"""
 
 import meep as mp
 import meep.materials as mt
@@ -10,8 +21,12 @@ import clipboard_and_style_sheet
 
 clipboard_and_style_sheet.style_sheet()
 
+clclt_dsprsn = False
+slc_sbstrt = True
+
 # %%____________________________________________________________________________________________________________________
-wl_min, wl_max = 1.4, 1.6
+# wl_min, wl_max = 1.4, 1.6
+wl_min, wl_max = 1 / np.array(mt.LiNbO3.valid_freq_range)[::-1]
 
 # %%____________________________________________________________________________________________________________________
 # Set up the geometry of the problem. One waveguide laid out in the x direction
@@ -22,7 +37,9 @@ wdth_wvgd = 0.5 * wl_wvgd / n_center_wvl  # width of the waveguide is half a wav
 dpml = 1  # PML thickness
 
 cntr_wvgd = mp.Vector3(0, 0, 0)  # where the waveguide is centered
-sx = 16  # size of the cell in the x direction
+sx = 0
+if not clclt_dsprsn:
+    sx += 16  # size of the cell in the x direction
 sy = 6  # size of the cell in y direction
 
 # %%____________________________________________________________________________________________________________________
@@ -41,21 +58,26 @@ PMLList = [PMLY]
 
 ABSX = mp.Absorber(dpml, direction=mp.X)
 ABSY = mp.Absorber(dpml, direction=mp.Y, side=mp.Low)
-ABSList = [ABSX, ABSY]
+ABSList = [ABSY]
+if not clclt_dsprsn:
+    # ABSList += [ABSX]
+    pass  # show periodic boundaries anyway
 
 # %%____________________________________________________________________________________________________________________
 # set the appropriate media for the geometric objects
 bw = np.array([1 / wl_max, 1 / wl_min])
 f_src = float(np.diff(bw) / 2 + bw[0])
 
-# blk1.material = mt.LiNbO3
-# blk2.material = mt.SiO2
-blk1.material = mp.Medium(epsilon=mt.LiNbO3.epsilon(f_src)[2, 2])
-blk2.material = mp.Medium(epsilon=mt.SiO2.epsilon(f_src)[2, 2])
+blk1.material = mt.LiNbO3
+blk2.material = mt.SiO2
+# blk1.material = mp.Medium(epsilon=mt.LiNbO3.epsilon(f_src)[2, 2])
+# blk2.material = mp.Medium(epsilon=mt.SiO2.epsilon(f_src)[2, 2])
 
 # %%____________________________________________________________________________________________________________________
 # create the geometry and boundary layers list
-geometry = [blk1, blk2]
+geometry = [blk1]
+if slc_sbstrt:
+    geometry += [blk2]
 boundary_layers = [*ABSList, *PMLList]
 
 # %%____________________________________________________________________________________________________________________
@@ -82,24 +104,45 @@ sim.use_output_directory('sim_output')
 
 # %%____________________________________________________________________________________________________________________
 # symmetries to exploit
-sim.symmetries = [mp.Mirror(direction=mp.X)]
+if not clclt_dsprsn:
+    sim.symmetries = [mp.Mirror(direction=mp.X)]
 
 # %%____________________________________________________________________________________________________________________
-sim.run(mp.to_appended("ez", mp.at_every(0.6, mp.output_efield_z)),
-        until_after_sources=mp.stop_when_fields_decayed(50, mp.Ez, pt, 1e-3))
+
+"""Calculate Waveguide Dispersion"""
+
+if clclt_dsprsn:
+    k_points = mp.interpolate(19, [mp.Vector3(1 / wl_max), mp.Vector3(1 / wl_min)])
+    freq = sim.run_k_points(300, k_points)
+
+    kx = np.array([i.x for i in k_points])
+    plt.figure()
+    plt.plot([kx.min(), kx.max()], [kx.min(), kx.max()], 'k', label='light line')
+    for n in range(len(freq)):
+        if len(freq[n]) > 0:
+            [plt.plot(kx[n], i.real, marker='o', color='C0') for i in freq[n]]
+    plt.legend(loc='best')
 
 # %%____________________________________________________________________________________________________________________
-# Done! Look at simulation results!
-with h5py.File('sim_output/2D_LiNbO3_wvgd-ez.h5', 'r') as f:
-    data = np.array(f[util.get_key(f)])
 
-# %%____________________________________________________________________________________________________________________
-# save = False
-# fig, ax = plt.subplots(1, 1)
-# for n in range(0, data.shape[2], 1):
-#     ax.clear()
-#     ax.imshow(data[:, ::-1, n].T, cmap='jet', vmax=np.max(data), vmin=data.min())
-#     if save:
-#         plt.savefig(f'../fig/{n}.png')
-#     else:
-#         plt.pause(.01)
+"""Visualize fields"""
+
+if not clclt_dsprsn:
+    sim.run(mp.to_appended("ez", mp.at_every(0.6, mp.output_efield_z)),
+            # until_after_sources=mp.stop_when_fields_decayed(50, mp.Ez, pt, 1e-3),
+            until_after_sources=300  # if you did periodic boundaries the fields don't decay much
+            )
+
+    # Done! Look at simulation results!
+    with h5py.File('sim_output/2D_LiNbO3_wvgd-ez.h5', 'r') as f:
+        data = np.array(f[util.get_key(f)])
+
+    save = False
+    fig, ax = plt.subplots(1, 1)
+    for n in range(0, data.shape[2], 1):
+        ax.clear()
+        ax.imshow(data[:, ::-1, n].T, cmap='jet', vmax=np.max(data), vmin=data.min())
+        if save:
+            plt.savefig(f'../fig/{n}.png')
+        else:
+            plt.pause(.01)
