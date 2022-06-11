0c82e226c9d56f3cb09f459588453d278254eb26
Peter <pech6593@colorado.edu>
2022-06-11 14:05:31 -0600

    latest update

--- scratch.py
+++ scratch.py
@@ -14,37 +14,83 @@ clipboard_and_style_sheet.style_sheet()
 
 class RidgeWaveGuide:
     def __init__(self, width, height, substrate_medium, waveguide_medium,
-                 resolution=64, num_bands=4, sy=2, sz=2):
+                 resolution=64, num_bands=4, cell_width=2, cell_height=2):
 
-        self.wdth = width
-        self.hght = height
-        self._sy = sy
-        self._sz = sz
+        self.lattice = mp.Lattice(size=mp.Vector3(0, cell_width, cell_height))
 
-        hght_sbstrt = (self._sz / 2) - (self.hght / 2)
-        offst_sbstrt = (hght_sbstrt / 2) + (self.hght / 2)
-
-        self.blk_wvgd = mp.Block(size=mp.Vector3(mp.inf, self.wdth, self.hght))
-        self.blk_sbstrt = mp.Block(size=mp.Vector3(mp.inf, mp.inf, hght_sbstrt),
-                                   center=mp.Vector3(0, 0, -offst_sbstrt))
+        self.blk_wvgd = mp.Block(size=mp.Vector3(mp.inf, width, height))
+        self.blk_sbstrt = mp.Block(size=mp.Vector3(mp.inf, mp.inf, self._hght_sbsrt),
+                                   center=mp.Vector3(0, 0, -self._z_offst_sbstrt))
 
         self.sbstrt_mdm = substrate_medium
         self.wvgd_mdm = waveguide_medium
 
-        self._lattice = mp.Lattice(size=mp.Vector3(0, self._sy, self._sz))
-        self._geometry = [self.blk_wvgd, self.blk_sbstrt]
+        self.geometry = [self.blk_wvgd, self.blk_sbstrt]
 
-        self.ms = mpb.ModeSolver(geometry_lattice=self._lattice,
-                                 geometry=self._geometry,
+        self.ms = mpb.ModeSolver(geometry_lattice=self.lattice,
+                                 geometry=self.geometry,
                                  resolution=resolution,
                                  num_bands=num_bands)
 
-        geometry_sim = copy.deepcopy(self._geometry)
+        self.redef_sim()
+
+    def redef_sim(self):
+        geometry_sim = copy.deepcopy(self.geometry)
         geometry_sim[0].material = mp.Medium(epsilon=self.wvgd_mdm.epsilon(1 / 1.55)[2, 2])
         geometry_sim[1].material = mp.Medium(epsilon=self.sbstrt_mdm.epsilon(1 / 1.55)[2, 2])
-        self.sim = mp.Simulation(cell_size=self._lattice.size,
+        self.sim = mp.Simulation(cell_size=self.lattice.size,
                                  geometry=geometry_sim,
-                                 resolution=resolution)
+                                 resolution=self.resolution[0])
+
+    def redef_sbstrt_dim(self):
+        self.blk_sbstrt.size.z = self._hght_sbsrt
+        self.blk_sbstrt.center.z = -self._z_offst_sbstrt
+
+    @property
+    def _hght_sbsrt(self):
+        return (self.cell_height / 2) - (self.height / 2)
+
+    @property
+    def _z_offst_sbstrt(self):
+        return (self._hght_sbsrt / 2) + (self.height / 2)
+
+    @property
+    def width(self):
+        return self.blk_wvgd.size.y
+
+    @width.setter
+    def width(self, width):
+        self.blk_wvgd.size.y = width
+        self.redef_sim()
+
+    @property
+    def height(self):
+        return self.blk_wvgd.size.z
+
+    @height.setter
+    def height(self, height):
+        self.blk_wvgd.size.z = height
+        self.redef_sbstrt_dim()
+        self.redef_sim()
+
+    @property
+    def cell_width(self):
+        return self.lattice.size.y
+
+    @cell_width.setter
+    def cell_width(self, sy):
+        self.lattice.size.y = sy
+        self.redef_sim()
+
+    @property
+    def cell_height(self):
+        return self.lattice.size.z
+
+    @cell_height.setter
+    def cell_height(self, sz):
+        self.lattice.size.z = sz
+        self.redef_sbstrt_dim()
+        self.redef_sim()
 
     @property
     def resolution(self):
@@ -53,6 +99,7 @@ class RidgeWaveGuide:
     @resolution.setter
     def resolution(self, resolution):
         self.ms.resolution = resolution
+        self.redef_sim()
 
     @property
     def num_bands(self):
@@ -64,7 +111,7 @@ class RidgeWaveGuide:
 
     @property
     def wvgd_mdm(self):
-        return self.blk_wvgd.material
+        return self._wvgd_mdm
 
     @wvgd_mdm.setter
     def wvgd_mdm(self, medium):
@@ -73,10 +120,11 @@ class RidgeWaveGuide:
         medium: mp.Medium
 
         self.blk_wvgd.material = medium
+        self._wvgd_mdm = medium
 
     @property
     def sbstrt_mdm(self):
-        return self.blk_sbstrt.material
+        return self._sbstrt_mdm
 
     @sbstrt_mdm.setter
     def sbstrt_mdm(self, medium):
@@ -85,17 +133,22 @@ class RidgeWaveGuide:
         medium: mp.Medium
 
         self.blk_sbstrt.material = medium
+        self._sbstrt_mdm = medium
 
     def plot2D(self):
         self.sim.plot2D()
 
     def calculate_dispersion(self, wl_min, wl_max, NPTS):
         if (self.wvgd_mdm.valid_freq_range[-1] == 1e20) and (self.sbstrt_mdm.valid_freq_range[-1] == 1e20):
-            return self._calc_dispersive(wl_min, wl_max, NPTS)
-        else:
+            # run a NON dispersive calculation
             return self._calc_non_dispersive(wl_min, wl_max, NPTS)
+        else:
+            # otherwise run a dispersive calculation
+            return self._calc_dispersive(wl_min, wl_max, NPTS)
 
     def _calc_dispersive(self, wl_min, wl_max, NPTS):
+        print("RUNNING A DISPERSIVE CALCULATION")
+
         k_points = mp.interpolate(NPTS, [mp.Vector3(1 / wl_max), mp.Vector3(1 / wl_min)])
         FREQ = np.zeros((len(k_points), self.num_bands))
         EPS_WVGD = np.zeros(len(k_points))
@@ -120,26 +173,57 @@ class RidgeWaveGuide:
         stop = time.time()
         print(f'finished after {(stop - start) / 60} minutes')
 
-        return np.array([i.x for i in k_points]), FREQ, EPS_WVGD, EPS_SBSTRT
+        class results:
+            def __init__(self):
+                self.kx = np.array([i.x for i in k_points])
+                self.freq = FREQ
+                self.eps_wvgd = EPS_WVGD
+                self.eps_sbstrt = EPS_SBSTRT
+
+        return results()
 
     def _calc_non_dispersive(self, wl_min, wl_max, NPTS):
+        print("RUNNING WITH A FIXED EPSILON")
+
         k_points = mp.interpolate(NPTS, [mp.Vector3(1 / wl_max), mp.Vector3(1 / wl_min)])
         self.ms.k_points = k_points
         self.ms.run()
-        return np.array([i.x for i in k_points]), self.ms.all_freqs
+
+        class results:
+            def __init__(self):
+                self.kx = np.array([i.x for i in k_points])
+
+        res = results()
+        res.freq = self.ms.all_freqs
+        return res
 
 
 # %%____________________________________________________________________________________________________________________
+wl_wvgd = 3.5  # um
+n_cntr_wl = mt.LiNbO3.epsilon((1 / wl_wvgd))[2, 2]  # ne polarization
+wdth_wvgd = 0.5 * wl_wvgd / n_cntr_wl
+
 ridge = RidgeWaveGuide(
-    width=.5,
-    height=.22,
-    substrate_medium=mp.Medium(index=1.45),
-    waveguide_medium=mp.Medium(index=3.45),
-    resolution=64,
+    width=wdth_wvgd,
+    height=.5,
+    # substrate_medium=mp.Medium(index=1.45),
+    # waveguide_medium=mp.Medium(index=3.45),
+    substrate_medium=mt.SiO2,
+    waveguide_medium=mt.LiNbO3,
+    resolution=30,
     num_bands=4,
-    sy=2,
-    sz=2
+    cell_width=8,
+    cell_height=8
 )
 
 # %%____________________________________________________________________________________________________________________
-kx, freq, eps_wvgd, eps_sbstrt = ridge.calculate_dispersion(1 / 2.0, 1 / .1, 20)
+res = ridge.calculate_dispersion(.4, 1.77, 19)
+
+# %%____________________________________________________________________________________________________________________
+plt.figure()
+[plt.plot(res.kx, res.freq[:, n], '.-') for n in range(res.freq.shape[1])]
+plt.plot(res.kx, res.kx, 'k', label='light line')
+plt.legend(loc='best')
+plt.xlabel("k ($\mathrm{\mu m}$)")
+plt.ylabel("$\mathrm{\\nu}$ ($\mathrm{\mu m}$)")
+plt.ylim(.25, 2.5)
