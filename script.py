8bedbe1d0f8ead5e25972d98e0212b0f92da4b39
Peter <pech6593@colorado.edu>
2022-06-14 22:47:00 -0600

    latest update (script.py)

--- script.py
+++ script.py
@@ -24,16 +24,21 @@ ridge = wg.RidgeWaveguide(
 
 # at 3 micron you need 5-6 bands (anyways, 4 was too small)
 # keep in mind you really just need 10 pts to do a prety good spline
-ridge.width = 5
-ridge.height = 5
-ridge.cell_width = 13
-ridge.cell_height = 13
-ridge.num_bands = 12
+# can roughly guide 3.9!
+ridge.width = 15
+ridge.height = 1.5
+ridge.cell_width = 30
+ridge.cell_height = 11.5
+ridge.num_bands = 15
 
-ridge.wvgd_mdm = mp.Medium(epsilon_diag=mt.LiNbO3.epsilon(1 / 1.55).diagonal())
-ridge.sbstrt_mdm = mp.Medium(epsilon_diag=mtp.Al2O3.epsilon(1 / 1.55).diagonal())
-res = ridge.calc_w_from_k(.4, 5, 15)
-# res = ridge.calc_dispersion(.8, 5, 10)
+# ridge.wvgd_mdm = mp.Medium(epsilon_diag=mt.LiNbO3.epsilon(1 / 1.55).diagonal())
+# ridge.sbstrt_mdm = mp.Medium(epsilon_diag=mtp.Al2O3.epsilon(1 / 1.55).diagonal())
+# res = ridge.calc_w_from_k(.4, 5, 15)
+res = ridge.calc_dispersion(.8, 5, 15)
 
 # %%____________________________________________________________________________________________________________________
 res.plot_dispersion()
+
+plot = lambda n: ridge.plot_mode(res.sm_bands[n], n)
+for n in range(len(res.kx)):
+    plot(n)
