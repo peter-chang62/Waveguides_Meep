a82afa0822e61d45492ae856295d1c1905633157
Peter <pech6593@colorado.edu>
2022-06-15 15:08:08 -0600

    latest update

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import meep.materials as mt
 import waveguide_dispersion as wg
 import materials as mtp
+import meep as mp
 
 ridge = wg.RidgeWaveguide(
     width=5,
@@ -16,16 +17,16 @@ ridge = wg.RidgeWaveguide(
 # at 3 micron you need 5-6 bands (anyways, 4 was too small)
 # keep in mind you really just need 10 pts to do a prety good spline
 # can roughly guide 3.9!
-ridge.width = 15
-ridge.height = 1.5
-ridge.cell_width = 30
-ridge.cell_height = 11.5
-ridge.num_bands = 15
+ridge.width = 1
+ridge.height = .7
+ridge.cell_width = 5
+ridge.cell_height = 5
+ridge.num_bands = 4
 
 # ridge.wvgd_mdm = mp.Medium(epsilon_diag=mt.LiNbO3.epsilon(1 / 1.55).diagonal())
 # ridge.sbstrt_mdm = mp.Medium(epsilon_diag=mtp.Al2O3.epsilon(1 / 1.55).diagonal())
 # res = ridge.calc_w_from_k(.4, 5, 15)
-res = ridge.calc_dispersion(.8, 5, 15)
+res = ridge.calc_dispersion(.8, 2, 15)
 
 # %%____________________________________________________________________________________________________________________
 res.plot_dispersion()
