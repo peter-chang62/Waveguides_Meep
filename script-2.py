e593428bd5eb8b24211c1f632228531b6194c542
Peter <pech6593@colorado.edu>
2022-06-16 23:54:28 -0600

    had changed the wrong sim parameters

--- script-2.py
+++ script-2.py
@@ -14,12 +14,13 @@ clipboard_and_style_sheet.style_sheet()
 sim = wg.ThinFilmWaveguide(3, .3, .7, mtp.Al2O3, mt.LiNbO3, 30, 1, 10, 4)
 
 # %%____________________________________________________________________________________________________________________
-etch_depth = wg.get_omega_axis(1 / .7, 1 / .3, 10)
+# etch_depth = wg.get_omega_axis(1 / .7, 1 / .3, 10)
 etch_width = wg.get_omega_axis(1 / 5, 1 / 3, 20)
+height = wg.get_omega_axis(1 / 1, 1 / 0.7, 10)
 for w in etch_width:
-    for d in etch_depth:
+    for h in height:
         sim.etch_width = w
-        sim.etch_depth = d
+        sim.height = h
 
         block_waveguide = sim.blk_wvgd  # save sim.blk_wvgd
         sim.blk_wvgd = geometry.convert_block_to_trapezoid(sim.blk_wvgd)  # set the blk_wvgd to a trapezoid
