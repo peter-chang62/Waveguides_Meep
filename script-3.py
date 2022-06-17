c022a29f3f0ece95400f314a22698d9fe8532257
Peter <pech6593@colorado.edu>
2022-06-17 17:43:44 -0600

    added script-3.py

---
+++ script-3.py
@@ -0,0 +1,54 @@
+"""sim data analysis """
+import numpy as np
+import matplotlib.pyplot as plt
+import clipboard_and_style_sheet
+import meep as mp
+import meep.materials as mt
+import materials as mtp
+import waveguide_dispersion as wg
+import os
+import geometry
+from pynlo.media.crystals.XTAL_PPLN import Gayer5PctSellmeier
+
+
+def width(s):
+    return float(s.split('_')[0])
+
+
+def height(s):
+    return float(s.split('_')[1].split('.npy')[0])
+
+
+# %%____________________________________________________________________________________________________________________
+path_disp = 'sim_output/06-16-2022/dispersion-curves/'
+name_disp = [i.name for i in os.scandir(path_disp)]
+name_disp = sorted(name_disp, key=height)
+name_disp = sorted(name_disp, key=width)
+
+get_disp = lambda n: np.load(path_disp + name_disp[n])
+plot_nu_disp = lambda n: plt.plot(get_disp(n)[:, 1], (get_disp(n)[:, 0] / get_disp(n)[:, 1]) ** 2, '.-')
+plot_wl_disp = lambda n: plt.plot(1 / get_disp(n)[:, 1], (get_disp(n)[:, 0] / get_disp(n)[:, 1]) ** 2, '.-')
+
+# %%____________________________________________________________________________________________________________________
+path_fields = 'sim_output/06-16-2022/E-fields/'
+name_fields = [i.name for i in os.scandir(path_fields)]
+name_fields = sorted(name_fields, key=height)
+name_fields = sorted(name_fields, key=width)
+
+get_field = lambda n: np.load(path_fields + name_fields[n])
+plot_field = lambda n, k_index, alpha=0.9: plt.imshow(get_field(n)[k_index, 0][::-1, ::-1].T, cmap='RdBu', alpha=alpha)
+
+# %%____________________________________________________________________________________________________________________
+path_eps = 'sim_output/06-16-2022/eps/'
+name_eps = [i.name for i in os.scandir(path_eps)]
+name_eps = sorted(name_eps, key=height)
+name_eps = sorted(name_eps, key=width)
+
+get_eps = lambda n: np.load(path_eps + name_eps[n])
+plot_eps = lambda n: plt.imshow(get_eps(n)[::-1, ::-1].T, interpolation='spline36', cmap='binary')
+
+# %%____________________________________________________________________________________________________________________
+plt.figure()
+[plot_nu_disp(i) for i in range(len(name_disp))]
+plt.xlabel("$\mathrm{\\nu \; (1 / \mu m)}$")
+plt.ylabel("$\mathrm{\\epsilon}$")
