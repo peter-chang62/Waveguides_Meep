752f970a6bdeef8c9bdb0d6de0ff506e28b80838
Peter <pech6593@colorado.edu>
2022-06-18 13:27:03 -0600

    update

---
+++ scratch-2.py
@@ -0,0 +1,11 @@
+"""complex transmission of a waveguide taper, you can set things up following mode-decomposition.py from
+meep/examples/ """
+
+import numpy as np
+import matplotlib.pyplot as plt
+import meep as mp
+import meep.materials as mt
+import materials as mtp
+import clipboard_and_style_sheet
+
+clipboard_and_style_sheet.style_sheet()
--- script-3.py
+++ script-3.py
@@ -26,8 +26,10 @@ name_disp = sorted(name_disp, key=height)
 name_disp = sorted(name_disp, key=width)
 
 get_disp = lambda n: np.load(path_disp + name_disp[n])
-plot_nu_disp = lambda n: plt.plot(get_disp(n)[:, 1], (get_disp(n)[:, 0] / get_disp(n)[:, 1]) ** 2, '.-')
-plot_wl_disp = lambda n: plt.plot(1 / get_disp(n)[:, 1], (get_disp(n)[:, 0] / get_disp(n)[:, 1]) ** 2, '.-')
+plot_nu_eps = lambda n: plt.plot(get_disp(n)[:, 1], (get_disp(n)[:, 0] / get_disp(n)[:, 1]) ** 2, '.-')
+plot_nu_vgrp = lambda n: plt.plot(get_disp(n)[:, 1], get_disp(n)[:, 2], '.-')
+plot_wl_eps = lambda n: plt.plot(1 / get_disp(n)[:, 1], (get_disp(n)[:, 0] / get_disp(n)[:, 1]) ** 2, '.-')
+plot_wl_vgrp = lambda n: plt.plot(1 / get_disp(n)[:, 1], get_disp(n)[:, 2], '.-')
 
 # %%____________________________________________________________________________________________________________________
 path_fields = 'sim_output/06-16-2022/E-fields/'
@@ -56,17 +58,22 @@ def plot_mode(n, k_index):
 
 # %%____________________________________________________________________________________________________________________
 plt.figure()
-[plot_nu_disp(i) for i in range(len(name_disp))]
+[plot_nu_eps(i) for i in range(len(name_disp))]
 plt.xlabel("$\mathrm{\\nu \; (1 / \mu m)}$")
 plt.ylabel("$\mathrm{\\epsilon}$")
 
+plt.figure()
+[plot_nu_vgrp(i) for i in range(len(name_disp))]
+plt.xlabel("$\mathrm{\\nu \; (1 / \mu m)}$")
+plt.ylabel("$\mathrm{v_{grp}}$")
+
 # %%____________________________________________________________________________________________________________________
-N = 115
-fig, ax = plt.subplots(1, 1)
-wl = 1 / get_disp(N)[:, 1]
-for n in range(26):
-    ax.clear()
-    plot_mode(N, n)
-    ax.set_title('%.3f' % wl[n] + " $\mathrm{\\mu m}$")
-    plt.savefig(f'fig/{n}.png')
-    # plt.pause(.1)
+# N = 115
+# fig, ax = plt.subplots(1, 1)
+# wl = 1 / get_disp(N)[:, 1]
+# for n in range(26):
+#     ax.clear()
+#     plot_mode(N, n)
+#     ax.set_title('%.3f' % wl[n] + " $\mathrm{\\mu m}$")
+#     # plt.savefig(f'fig/{n}.png')
+#     plt.pause(.1)
