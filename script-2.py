c2e3f7b20de2906be2c664e17393b8a284648ca5
Peter <pech6593@colorado.edu>
2022-07-18 17:06:19 -0600

    vary the etch depth

--- script-2.py
+++ script-2.py
@@ -17,16 +17,29 @@ clipboard_and_style_sheet.style_sheet()
 eps_func_wvgd = lambda omega: Gayer5PctSellmeier(24.5).n((1 / omega) * 1e3) ** 2
 
 # %%____________________________________________________________________________________________________________________
-sim = wg.ThinFilmWaveguide(3, .3, .7, mtp.Al2O3, mt.LiNbO3, 30, 1, 10, 4)
+sim = wg.ThinFilmWaveguide(etch_width=3,
+                           etch_depth=.3,
+                           # film_thickness=.7,
+                           film_thickness=1,  # I'll fix the height at 1 um now
+                           substrate_medium=mtp.Al2O3,
+                           waveguide_medium=mt.LiNbO3,
+                           resolution=30,
+                           num_bands=1,
+                           cell_width=10,
+                           cell_height=4)
 
 # %%____________________________________________________________________________________________________________________
 etch_width = wg.get_omega_axis(1 / 3, 1 / 0.3, 20)  # 300 nm to 3 um in 135 nm steps
 # height = wg.get_omega_axis(1 / 1, 1 / 0.7, 10)  # 700 nm to 1 um in 30 nm steps
-height = np.arange(1.05, 3.05, .05)  # continue parameter sweep: 1050 nm to 3000 nm in 50 nm steps
+# height = np.arange(1.05, 3.05, .05)  # continue parameter sweep: 1050 nm to 3000 nm in 50 nm steps
+etch_depth = np.arange(0.1, 1.05, .05)
+etch_depth = np.round(etch_depth, 2)
 for w in etch_width:
-    for h in height:
+    # for h in height:
+    for d in etch_depth:
         sim.etch_width = w
-        sim.height = h
+        # sim.height = h
+        sim.etch_depth = d
 
         block_waveguide = sim.blk_wvgd  # save sim.blk_wvgd
         sim.blk_wvgd = geometry.convert_block_to_trapezoid(sim.blk_wvgd)  # set the blk_wvgd to a trapezoid
@@ -34,6 +47,10 @@ for w in etch_width:
         sim.blk_wvgd = block_waveguide  # reset trapezoid back to blk_wvgd
 
         arr = np.c_[res.kx, res.freq, res.v_g[:, 0, 0]]  # 0, 0 -> first band, x-component (non-zero component)
-        np.save(f'sim_output/06-16-2022/dispersion-curves/{w}_{h}.npy', arr)
-        np.save(f'sim_output/06-16-2022/E-fields/{w}_{h}.npy', sim.E[:, :, :, :, 1].__abs__() ** 2)
-        np.save(f'sim_output/06-16-2022/eps/{w}_{h}.npy', sim.ms.get_epsilon())
+        # np.save(f'sim_output/06-16-2022/dispersion-curves/{w}_{h}.npy', arr)
+        # np.save(f'sim_output/06-16-2022/E-fields/{w}_{h}.npy', sim.E[:, :, :, :, 1].__abs__() ** 2)
+        # np.save(f'sim_output/06-16-2022/eps/{w}_{h}.npy', sim.ms.get_epsilon())
+
+        np.save(f'sim_output/07-18-2022/dispersion-curves/{w}_{d}.npy', arr)
+        np.save(f'sim_output/07-18-2022/E-fields/{w}_{d}.npy', sim.E[:, :, :, :, 1].__abs__() ** 2)
+        np.save(f'sim_output/07-18-2022/eps/{w}_{d}.npy', sim.ms.get_epsilon())
