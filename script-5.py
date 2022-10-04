84b5fffb110c7e36769844c00caca145b08ba1b8
Peter <pech6593@colorado.edu>
2022-10-03 18:02:02 -0600

    update

--- script-5.py
+++ script-5.py
@@ -49,8 +49,9 @@ eps_func_wvgd = lambda omega: Gayer5PctSellmeier(24.5).n((1 / omega) * 1e3) ** 2
 # %%____________________________________________________________________________________________________________________
 sim = wg.ThinFilmWaveguide(etch_width=3,
                            etch_depth=.3,
-                           film_thickness=1,  # I'll fix the height at 1 um now
-                           substrate_medium=mp.Medium(index=1),
+                           film_thickness=1,
+                           # substrate_medium=mp.Medium(index=1),
+                           substrate_medium=mtp.Al2O3,
                            waveguide_medium=mt.LiNbO3,
                            resolution=30,
                            num_bands=1,
@@ -59,43 +60,43 @@ sim = wg.ThinFilmWaveguide(etch_width=3,
 
 # %%____________________________________________________________________________________________________________________
 # # single waveguide parameter (comment out if running the for loop block instead)
-# sim.etch_width = 2.5
-# sim.etch_depth = 0.6
-#
-# block_waveguide = sim.blk_wvgd  # save sim.blk_wvgd
-# sim.blk_wvgd = geometry.convert_block_to_trapezoid(sim.blk_wvgd)  # set the blk_wvgd to a trapezoid
-# res = sim.calc_dispersion(.8, 5, 50, eps_func_wvgd=eps_func_wvgd)  # run simulation
-# sim.blk_wvgd = block_waveguide  # reset trapezoid back to blk_wvgd
+sim.etch_width = 3.0
+sim.etch_depth = 0.70
 
-# wl, omega, beta, beta1, beta2 = get_beta(res)
-# plot_mode(res, sim, 0, 0)
+block_waveguide = sim.blk_wvgd  # save sim.blk_wvgd
+sim.blk_wvgd = geometry.convert_block_to_trapezoid(sim.blk_wvgd)  # set the blk_wvgd to a trapezoid
+res = sim.calc_dispersion(.8, 5, 50, eps_func_wvgd=eps_func_wvgd)  # run simulation
+sim.blk_wvgd = block_waveguide  # reset trapezoid back to blk_wvgd
+
+wl, omega, beta, beta1, beta2 = get_beta(res)
+plot_mode(res, sim, 0, 0)
 
 # %%____________________________________________________________________________________________________________________
 # for loop sweep through parameters
 # I've verified that these are all guided (really only had to check 2.5 x .3 um)
-etch_width = wg.get_omega_axis(1 / 5, 1 / 2.5, 5)
-etch_depth = wg.get_omega_axis(1 / .6, 1 / .3, 6)
-NPTS = len(etch_width) * len(etch_depth)
-
-h = 0
-for w in etch_width:
-    # for h in height:
-    for d in etch_depth:
-        sim.etch_width = w
-        sim.etch_depth = d
-
-        block_waveguide = sim.blk_wvgd  # save sim.blk_wvgd
-        sim.blk_wvgd = geometry.convert_block_to_trapezoid(sim.blk_wvgd)  # set the blk_wvgd to a trapezoid
-        res = sim.calc_dispersion(.8, 5, 50, eps_func_wvgd=eps_func_wvgd)  # run simulation
-        sim.blk_wvgd = block_waveguide  # reset trapezoid back to blk_wvgd
-
-        wl, omega, beta, beta1, beta2 = get_beta(res, False)
-        arr = np.c_[res.freq, beta, beta1, beta2]
-
-        # _______________________________________ save the data ________________________________________________________
-        np.save(f'sim_output/09-08-2022/dispersion-curves/{w}_{d}.npy', arr)
-        np.save(f'sim_output/09-08-2022/E-fields/{w}_{d}.npy', sim.E[:, :, :, :, 1].__abs__() ** 2)
-        np.save(f'sim_output/09-08-2022/eps/{w}_{d}.npy', sim.ms.get_epsilon())
-
-        h += 1
-        print(f'______________________________________finished {h} of {NPTS}________________________________')
+# etch_width = wg.get_omega_axis(1 / 5, 1 / 2.5, 5)
+# etch_depth = wg.get_omega_axis(1 / .6, 1 / .3, 6)
+# NPTS = len(etch_width) * len(etch_depth)
+#
+# h = 0
+# for w in etch_width:
+#     # for h in height:
+#     for d in etch_depth:
+#         sim.etch_width = w
+#         sim.etch_depth = d
+#
+#         block_waveguide = sim.blk_wvgd  # save sim.blk_wvgd
+#         sim.blk_wvgd = geometry.convert_block_to_trapezoid(sim.blk_wvgd)  # set the blk_wvgd to a trapezoid
+#         res = sim.calc_dispersion(.8, 5, 50, eps_func_wvgd=eps_func_wvgd)  # run simulation
+#         sim.blk_wvgd = block_waveguide  # reset trapezoid back to blk_wvgd
+#
+#         wl, omega, beta, beta1, beta2 = get_beta(res, False)
+#         arr = np.c_[res.freq, beta, beta1, beta2]
+#
+#         # _______________________________________ save the data ______________________________________________________
+#         np.save(f'sim_output/09-08-2022/dispersion-curves/{w}_{d}.npy', arr)
+#         np.save(f'sim_output/09-08-2022/E-fields/{w}_{d}.npy', sim.E[:, :, :, :, 1].__abs__() ** 2)
+#         np.save(f'sim_output/09-08-2022/eps/{w}_{d}.npy', sim.ms.get_epsilon())
+#
+#         h += 1
+#         print(f'______________________________________finished {h} of {NPTS}________________________________')
--- script-7.py
+++ script-7.py
@@ -6,26 +6,91 @@ First attempt to run waveguide dispersion parameters through Connor's PyNLO clas
 import numpy as np
 from scipy.constants import pi, c
 from matplotlib import pyplot as plt
+from scipy.interpolate import interp1d
+import pynlo_connor as pynlo
+from pynlo_connor import utility as utils
 
-from PyNLO_Connor import pynlo
-from PyNLO_Connor.pynlo import utility as utils
+# importing dispersion data ____________________________________________________________________________________________
+b_data = np.load("sim_output/10-03-2022/3x.7_wl_omega_b_b1_b2.npy")
+wl, omega, b, b1, b2 = b_data[:, 0], b_data[:, 1], b_data[:, 2], b_data[:, 3], b_data[:, 4]
+k = b * 1e6 / (2 * np.pi)  # 1 / m
+freq = c / (wl * 1e-6)  # Hz
+n = c * k / freq  # n = k * c / freq
+n_wvgd = interp1d(freq, n, kind='cubic', bounds_error=True)
 
-# %% Pulse Properties
-
-# Pulse Properties _____________________________________________________________________________________________________
+# %% Pulse Properties __________________________________________________________________________________________________
 n_points = 2 ** 13
-v_min = c / 3500e-9  # c / 3500 nm
-v_max = c / 450e-9  # c / 900 nm
-v0 = c / 1550e-9  # c / 1550 nm
+v_min = c / 4500e-9  # c / 4500 nm
+v_max = c / 800e-9  # c / 815 nm
+v0 = c / 1560 - 9  # c / 1550 nm
 e_p = 1e-9  # 1 nJ
 t_fwhm = 50e-15  # 50 fs
 
 pulse = pynlo.light.Pulse.Sech(n_points, v_min, v_max, v0, e_p, t_fwhm)
-pulse.rtf_grids(n_harmonic=2, update=True)  # anti-aliasing
+pulse.rtf_grids(n_harmonic=1, update=True)  # anti-aliasing
 
 v_grid = pulse.v_grid
 t_grid = pulse.t_grid
 
 # %% ___________________________________________________________________________________________________________________
 # Waveguide properties
-length = 10e-3  # 10 mm
+length = 20e-3  # 10 mm
+a_eff = 2.56e-12
+
+n_eff = n_wvgd(v_grid)
+beta = n_eff * 2 * np.pi * v_grid / c  # n * w / c
+
+# 2nd order nonlinearity
+d_eff = 27e-12  # 27 pm / V
+chi2_eff = 2 * d_eff
+g2 = utils.chi2.g2_shg(v0, v_grid, n_eff, a_eff, chi2_eff)
+
+# 3rd order nonlinearity
+chi3_eff = 5200e-24
+g3 = utils.chi3.g3_spm(n_eff, a_eff, chi3_eff)
+
+# Mode
+mode = pynlo.media.Mode(
+    v_grid=v_grid,
+    beta_v=beta,
+    g2_v=g2,
+    g2_inv=None,
+    g3_v=g3,
+    z=0.0
+)
+
+# Model
+model = pynlo.model.SM_UPE(pulse, mode)
+local_error = 1e-6
+dz = model.estimate_step_size(n=20, local_error=local_error)
+
+# Simulate
+z_grid = np.linspace(0, length, 100)
+pulse_out, z, a_t, a_v = model.simulate(z_grid, dz=dz, local_error=local_error, n_records=100, plot=None)
+
+fig = plt.figure("Simulation Results", clear=True)
+ax0 = plt.subplot2grid((3, 2), (0, 0), rowspan=1)
+ax1 = plt.subplot2grid((3, 2), (0, 1), rowspan=1)
+ax2 = plt.subplot2grid((3, 2), (1, 0), rowspan=2, sharex=ax0)
+ax3 = plt.subplot2grid((3, 2), (1, 1), rowspan=2, sharex=ax1)
+
+p_v_dB = 10 * np.log10(np.abs(a_v) ** 2)
+p_v_dB -= p_v_dB.max()
+ax0.plot(1e-12 * v_grid, p_v_dB[0], color="b")
+ax0.plot(1e-12 * v_grid, p_v_dB[-1], color="g")
+ax2.pcolormesh(1e-12 * v_grid, 1e3 * z, p_v_dB, vmin=-40.0, vmax=0, shading="auto")
+ax0.set_ylim(bottom=-50, top=10)
+ax2.set_xlabel('Frequency (THz)')
+
+p_t_dB = 10 * np.log10(np.abs(a_t) ** 2)
+p_t_dB -= p_t_dB.max()
+ax1.plot(1e12 * t_grid, p_t_dB[0], color="b")
+ax1.plot(1e12 * t_grid, p_t_dB[-1], color="g")
+ax3.pcolormesh(1e12 * t_grid, 1e3 * z, p_t_dB, vmin=-40.0, vmax=0, shading="auto")
+ax1.set_ylim(bottom=-50, top=10)
+ax3.set_xlabel('Time (ps)')
+
+ax0.set_ylabel('Power (dB)')
+ax2.set_ylabel('Propagation Distance (mm)')
+fig.tight_layout()
+fig.show()
