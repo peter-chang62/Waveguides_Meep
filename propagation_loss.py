4f043de46ed498f2ff9e311e673599bd8a87bc66
Peter <pech6593@colorado.edu>
2023-03-21 18:18:52 -0600

    latest calculations

--- propagation_loss.py
+++ propagation_loss.py
@@ -10,14 +10,6 @@ import pynlo_extras as pe
 from scipy import constants as sc
 
 
-# %% dimensions
-etch_width = 3
-etch_depth = 0.3
-film_thickness = 0.630
-
-wl_min = 3
-wl_max = 5
-
 # %% materials
 ppln = pe.materials.PPLN()
 
@@ -33,6 +25,9 @@ substrate_medium = mtp.Al2O3
 waveguide_medium = mt.LiNbO3
 
 # %% create waveguide
+etch_width = 3
+etch_depth = 0.3
+film_thickness = 0.630
 TFW = wg.ThinFilmWaveguide(
     etch_width,
     etch_depth,
@@ -45,41 +40,94 @@ TFW = wg.ThinFilmWaveguide(
     num_bands=1,
 )
 
-
 # %% run simulation to obtain a list of k_points the unguided modes are those
 #    that lie above the light line, which are the ones that we want to solve
-#    for propagation loss
+#    for propagation loss. You might want to run this simulation separately
+#    because MPB is not meant to be called with multiple processes using
+#    mpirun (it'll take longer!)
+# wl_min = 3
+# wl_max = 5
 # res = TFW.calc_dispersion(
 #     wl_min, wl_max, 100, eps_func_wvgd=eps_func_wvgd, eps_func_sbstrt=None
 # )
 
 # (unguided,) = (res.freq > np.squeeze(res.kx) / res._index_sbstrt).nonzero()
-# unguided = np.arange(len(unguided))  # add a few more
-
-# # dispersion = np.c_[res.freq[unguided], np.squeeze(res.kx)[unguided]]
-# dispersion = np.c_[res.freq, np.squeeze(res.kx)]
-# np.save("dispersion.npy", dispersion)
+# k_points = np.c_[res.freq, np.squeeze(res.kx)]
+# np.save("k_points.npy", k_points)
+# np.save("unguided_index.npy", unguided)
 
 # ----------------------- propagation loss calculation ------------------------
 
-# %% get sim
-dispersion = np.load("dispersion.npy")
-sim = TFW.sim
+# %% add pml layers
+dpml = 1
 pml_layers = [
-    mp.PML(thickness=1, direction=mp.Z),
-    mp.PML(thickness=1, direction=mp.Y),
+    mp.PML(thickness=dpml, direction=mp.Z),
+    mp.PML(thickness=dpml, direction=mp.Y),
 ]
+TFW.cell_width += dpml * 2  # extend the cell for the pml layers
+TFW.cell_height += dpml * 2
+sim = TFW.sim
 sim.boundary_layers = pml_layers
 
-# %% run simulation for each k_point
+k_points = np.load("k_points.npy")
+unguided = np.load("unguided_index.npy")
+
+# %% run for unguided frequencies, index from high v to low v, truncating when
+#    the loss is too high
+# FREQ = []
+# DECAY = []
+# for fcen, kx in k_points[unguided][::-1]:
+#     # reset
+#     sim.reset_meep()
+
+#     # sources
+#     src = mp.GaussianSource(frequency=fcen, width=10)
+#     sources = [mp.Source(src, mp.Ey, center=mp.Vector3())]
+#     sim.sources = sources
+
+#     # set waveguide epsilon
+#     eps_wvgd = eps_func_wvgd(fcen)
+#     TFW.blk_wvgd.material = mp.Medium(epsilon=eps_wvgd)
+#     TFW._blk_film.material = mp.Medium(epsilon=eps_wvgd)
+
+#     # set substrate epsilon
+#     eps_sbstrt = TFW.sbstrt_mdm.epsilon(fcen)[2, 2]
+#     TFW.blk_sbstrt.material = mp.Medium(epsilon=eps_sbstrt)
+#     sim.geometry = TFW.geometry
+
+#     # harminv monitoring: offset from the symmetry plane slightly in the
+#     # horizontal direction
+#     df = 100e-3 / (1 / fcen) ** 2  # 100 nm
+#     h = mp.Harminv(mp.Ey, mp.Vector3(0, 0.1234, 0), fcen, df)
+
+#     k_point = mp.Vector3(kx, 0, 0)
+#     sim.k_point = k_point
+#     sim.run(mp.after_sources(h), until_after_sources=300)
+
+#     if len(h.modes) == 0:
+#         break
+#     else:
+#         # for propagation loss, I work close to the cut off frequency, there
+#         # shouldn't be other modes close by
+#         assert len(h.modes) == 1
+#         (mode,) = h.modes
+#         re = mode.freq
+#         im = abs(mode.decay)
+#         FREQ.append(re)
+#         DECAY.append(im)
+# unguided_dispersion = np.c_[FREQ, DECAY]
+# np.save("unguided_dispersion.npy", unguided_dispersion)
+
+# %% run for guided frequencies, index from low v to high v, truncating when
+#    the loss is sufficiently low
 FREQ = []
 DECAY = []
-for fcen, kx in dispersion[::-1]:
+for fcen, kx in k_points[unguided[-1] + 1 :]:
     # reset
     sim.reset_meep()
 
     # sources
-    src = mp.GaussianSource(frequency=fcen, width=5)
+    src = mp.GaussianSource(frequency=fcen, width=10)
     sources = [mp.Source(src, mp.Ey, center=mp.Vector3())]
     sim.sources = sources
 
@@ -102,44 +150,20 @@ for fcen, kx in dispersion[::-1]:
     sim.k_point = k_point
     sim.run(mp.after_sources(h), until_after_sources=300)
 
-    if len(h.modes) == 0:
+    # for propagation loss, I work close to the cut off frequency, there
+    # shouldn't be other modes close by
+    assert len(h.modes) == 1
+    (mode,) = h.modes
+    re = mode.freq
+    im = abs(mode.decay)
+    FREQ.append(re)
+    DECAY.append(im)
+
+    if im <= 1e-5:
         break
-    else:
-        assert len(h.modes) == 1
-        (mode,) = h.modes
-        re = mode.freq
-        im = mode.decay
-        FREQ.append(re)
-        DECAY.append(abs(im))
 
-arr = np.c_[FREQ, DECAY]
-np.save("freq_decay.npy", arr)
+guided_dispersion = np.c_[FREQ, DECAY]
+np.save("guided_dispersion.npy", guided_dispersion)
 
-# %% --------------------- sort stuff out -------------------------------------
-
-# guided starts from the next one after the last in unguided
-# guided = np.arange(len(res.freq))[unguided[-1] + 1 :]
-# guided_freq = res.freq[guided]
-# unguided_freq = res.freq[unguided]
-
-# # truncate at frequencies below which loss was too high to calculate
-# # index high -> low and truncate at low, then re-index low -> high
-# unguided_freq = unguided_freq[::-1][: len(DECAY)][::-1]
-
-# # DECAY was indexed high -> low
-# decay = np.zeros(len(unguided_freq))
-# decay[:] = np.asarray(DECAY)
-# decay = decay[::-1]
-# decay = np.append(decay, np.zeros(len(guided_freq)))
-
-# # savable simulation data!
-# N_throw = len(res.freq) - len(decay)
-# arr = np.c_[res.freq[N_throw:], res.kx.flatten()[N_throw:], decay]
-
-# um = 1e-6
-# v_grid = res.freq * sc.c / um
-# w_grid = v_grid * 2 * np.pi
-# b = 2 * np.pi * res.kx.flatten() * 1 / um
-# b_1 = np.gradient(b, w_grid, edge_order=2)
-
-# decay = arr[:, 2] * b_1[: len(arr)] * sc.c / um
+# dispersion = np.vstack([unguided_dispersion[::-1], guided_dispersion])
+# np.save("dispersion.npy", dispersion)
