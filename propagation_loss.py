020dea1fa377896c0432d5ef37790be37b1f94ec
peter chang <pech6593@colorado.edu>
2024-03-19 11:18:56 -0600

    deleting old propagation loss files

--- propagation_loss.py
+++ propagation_loss.py
@@ -1,209 +1,206 @@
-# %% package imports
+# %% ----- package imports
 import numpy as np
 import matplotlib.pyplot as plt
-import clipboard_and_style_sheet
-from TFW_meep import waveguide_dispersion as wg
-from TFW_meep import materials as mtp
-from TFW_meep import geometry
-from meep import materials as mt
 import meep as mp
-import pynlo_extras as pe
-from scipy import constants as sc
+from meep import materials as mt
+from TFW_meep import materials as mtp
+from TFW_meep import waveguide_dispersion as wg
+import scipy.constants as sc
+from TFW_meep import geometry
+from scipy.interpolate import UnivariateSpline
+import copy
+from scipy.constants import c
+import clipboard
 
 
-# %% materials
-ppln = pe.materials.PPLN()
-
-
-def eps_func_wvgd(freq):
+# %% ----- Gayer paper Sellmeier equation for ne (taken from PyNLO 1 / omega is in um
+# n = squrt[epsilon] so epsilon = n^2
+def eps_func_wvgd(omega):
+    # omega is in inverse micron
     um = 1e-6
+    v = omega * sc.c / um
+    return n_MgLN_G(v, T=24.5, axis="e") ** 2
 
-    nu = freq * sc.c / um
-    return ppln.n(nu) ** 2
+
+def n_MgLN_G(v, T=24.5, axis="e"):
+    """
+    Range of Validity:
+        - 500 nm to 4000 nm
+        - 20 C to 200 C
+        - 48.5 mol % Li
+        - 5 mol % Mg
+
+    Gayer, O., Sacks, Z., Galun, E. et al. Temperature and wavelength
+    dependent refractive index equations for MgO-doped congruent and
+    stoichiometric LiNbO3 . Appl. Phys. B 91, 343–348 (2008).
+
+    https://doi.org/10.1007/s00340-008-2998-2
+
+    """
+    if axis == "e":
+        a1 = 5.756  # plasmons in the far UV
+        a2 = 0.0983  # weight of UV pole
+        a3 = 0.2020  # pole in UV
+        a4 = 189.32  # weight of IR pole
+        a5 = 12.52  # pole in IR
+        a6 = 1.32e-2  # phonon absorption in IR
+        b1 = 2.860e-6
+        b2 = 4.700e-8
+        b3 = 6.113e-8
+        b4 = 1.516e-4
+    elif axis == "o":
+        a1 = 5.653  # plasmons in the far UV
+        a2 = 0.1185  # weight of UV pole
+        a3 = 0.2091  # pole in UV
+        a4 = 89.61  # weight of IR pole
+        a5 = 10.85  # pole in IR
+        a6 = 1.97e-2  # phonon absorption in IR
+        b1 = 7.941e-7
+        b2 = 3.134e-8
+        b3 = -4.641e-9
+        b4 = -2.188e-6
+
+    else:
+        raise ValueError("axis needs to be o or e")
+
+    wvl = sc.c / v * 1e6  # um
+    f = (T - 24.5) * (T + 570.82)
+    n2 = (
+        (a1 + b1 * f)
+        + (a2 + b2 * f) / (wvl**2 - (a3 + b3 * f) ** 2)
+        + (a4 + b4 * f) / (wvl**2 - a5**2)
+        - a6 * wvl**2
+    )
+    return n2**0.5
 
 
-substrate_medium = mtp.Al2O3
-waveguide_medium = mt.LiNbO3
-
-# %% create waveguide
-etch_width = 1.65
-etch_depth = 0.7
-film_thickness = 1.0
-TFW = wg.ThinFilmWaveguide(
-    etch_width,
-    etch_depth,
-    film_thickness,
-    substrate_medium,
-    waveguide_medium,
-    resolution=30,
-    cell_width=8,
-    cell_height=6,
+# %% ----- create sim instance ------------------------------------------------
+etch_width = 1.3
+etch_depth = 0.2
+resolution = 30
+sim = wg.ThinFilmWaveguide(
+    etch_width=etch_width,
+    etch_depth=etch_depth,
+    film_thickness=0.4,
+    substrate_medium=mt.SiO2,
+    waveguide_medium=mt.LiNbO3,
+    resolution=resolution,
+    cell_width=10,
+    cell_height=10,
+    z_offset_wvgd=1.5,
     num_bands=1,
 )
 
-# %% run simulation to obtain a list of k_points the unguided modes are those
-#    that lie above the light line, which are the ones that we want to solve
-#    for propagation loss. You might want to run this simulation separately
-#    because MPB is not meant to be called with multiple processes using
-#    mpirun (it'll take longer!)
-wl_min = 3
-wl_max = 5
-res = TFW.calc_dispersion(
-    wl_min, wl_max, 100, eps_func_wvgd=eps_func_wvgd, eps_func_sbstrt=None
-)
+# %% ----- waveguide simulation -----------------------------------------------
+res = sim.calc_dispersion(0.4, 1.77, 100, eps_func_wvgd=eps_func_wvgd)  # simulate
 
-(unguided,) = (res.freq > np.squeeze(res.kx) / res._index_sbstrt).nonzero()
-k_points = np.c_[res.freq, np.squeeze(res.kx)]
-np.save("k_points.npy", k_points)
-np.save("unguided_index.npy", unguided)
+wl = 1 / res.freq
+um = 1e-6
+omega = res.freq * 2 * np.pi * c / um
+# conversion = sc.c**-2 * 1e12**2 * 1e3**2 * 1e-9
+beta = res.kx.flatten() * 2 * np.pi / um
+beta1 = np.gradient(beta, omega, edge_order=2)
+beta2 = np.gradient(beta1, omega, edge_order=2)
 
-# ----------------------- propagation loss calculation ------------------------
+# %% ----- prep time domain sim -----------------------------------------------
+# fcen = 1 / 3.8
+# df = 0.1
 
-# %% add pml layers
-dpml = 1
-pml_layers = [
-    mp.PML(thickness=dpml, direction=mp.Z),
-    mp.PML(thickness=dpml, direction=mp.Y),
-]
-TFW.cell_width += dpml * 2  # extend the cell for the pml layers
-TFW.cell_height += dpml * 2
+# blk_wvgd, blk_sbstrt, blk_film = copy.deepcopy(sim.geometry)
+# blk_wvgd = geometry.convert_block_to_trapezoid(blk_wvgd, angle_deg=80)
+# blk_film.size.y = sim.lattice.size.y
+# blk_sbstrt.size.y = sim.lattice.size.y
 
-# make sure to account for the etch angle! No wonder your frequencies weren't
-# matching up that well between MPB and Harminv!
-blk_wvgd = TFW.blk_wvgd
-TFW.blk_wvgd = geometry.convert_block_to_trapezoid(TFW.blk_wvgd, angle_deg=80)
+# blk_wvgd.material = mp.Medium(epsilon=eps_func_wvgd(fcen))
+# blk_film.material = mp.Medium(epsilon=eps_func_wvgd(fcen))
+# blk_sbstrt.material = mp.Medium(epsilon=sim.sbstrt_mdm.epsilon(fcen).diagonal()[-1])
 
-sim = TFW.sim
-sim.boundary_layers = pml_layers
+# geometry = [blk_wvgd, blk_film, blk_sbstrt]
 
-k_points = np.load("k_points.npy")
-unguided = np.load("unguided_index.npy")
+# dpml = 3
+# PML = [mp.PML(dpml, mp.Y), mp.PML(dpml, mp.Z)]
 
-# %% run for unguided frequencies, index from high v to low v, truncating when
-#    the loss is too high
-FREQ = []
-DECAY = []
-for fcen, kx in k_points[unguided][::-1]:
-    # ------ reset
-    sim.reset_meep()
+# length = 0  # 1 um
+# cell_size = mp.Vector3(
+#     length, sim.lattice.size.y + dpml * 2, sim.lattice.size.z + dpml * 2
+# )
+# if length > 0:
+#     blk_wvgd.height = length
+#     blk_wvgd.center.x = 0
 
-    k_point = mp.Vector3(kx, 0, 0)
+# sources = [
+#     mp.EigenModeSource(
+#         src=mp.GaussianSource(frequency=fcen, fwidth=df),
+#         center=mp.Vector3(0, 0, 0),
+#         size=mp.Vector3(
+#             0,
+#             sim.lattice.size.y,
+#             sim.lattice.size.z,
+#         ),
+#         eig_match_freq=True,
+#         eig_parity=mp.ODD_Y,
+#     )
+# ]
 
-    # ------ sources
-    src = mp.GaussianSource(frequency=fcen, width=10)
-    # mp.NO_DIRECTION: eig_k_point is initial guess, search direction, and sets
-    # the direction of the waveguide
-    eigenmode = mp.EigenModeSource(
-        src,
-        center=mp.Vector3(),
-        # the source extends the cell, not including pml layers
-        size=mp.Vector3(
-            0,
-            TFW.cell_width - dpml * 2 - 1,
-            TFW.cell_height - dpml * 2 - 1,
-        ),
-        direction=mp.NO_DIRECTION,
-        eig_band=1,  # maybe it wasn't using the first band?
-        eig_kpoint=k_point,
-        eig_match_freq=True,
-    )
-    # sources = [mp.Source(src, mp.Ey, center=mp.Vector3())]  # point source
-    sources = [eigenmode]
-    sim.sources = sources
+# # resolution = (1 / fcen / eps_func_wvgd(fcen) / 10) ** -1
+# resolution = 20
 
-    # ------ set waveguide epsilon
-    eps_wvgd = eps_func_wvgd(fcen)
-    TFW.blk_wvgd.material = mp.Medium(epsilon=eps_wvgd)
-    TFW._blk_film.material = mp.Medium(epsilon=eps_wvgd)
+# sim_td = mp.Simulation(
+#     cell_size=cell_size,
+#     geometry=geometry,
+#     sources=sources,
+#     boundary_layers=PML,
+#     resolution=resolution,
+#     symmetries=[mp.Mirror(mp.Y, phase=-1)],
+# )
 
-    # ------ set substrate epsilon
-    eps_sbstrt = TFW.sbstrt_mdm.epsilon(fcen)[2, 2]
-    TFW.blk_sbstrt.material = mp.Medium(epsilon=eps_sbstrt)
-    sim.geometry = TFW.geometry
+# %% ------ monitors ----------------------------------------------------------
+# nonpml_vol = mp.Volume(
+#     center=mp.Vector3(),
+#     size=mp.Vector3(
+#         length,
+#         sim.lattice.size.y,
+#         sim.lattice.size.z,
+#     ),
+# )
+# dft_obj = sim_td.add_dft_fields([mp.Ey], np.array([fcen]), where=nonpml_vol)
 
-    # ------ harminv monitoring
-    df = 100e-3 / (1 / fcen) ** 2  # 100 nm
-    h = mp.Harminv(mp.Ey, mp.Vector3(0, 0, 0), fcen, df)
+# %% ------ run FDTD ----------------------------------------------------------
+# Iy = []
 
-    # ------ run simulation
-    sim.k_point = k_point
-    sim.run(mp.after_sources(h), until_after_sources=300)
 
-    if len(h.modes) == 0:
-        break
-    else:
-        # for propagation loss, I work close to the cut off frequency, there
-        # shouldn't be other modes close by
-        assert len(h.modes) == 1
-        (mode,) = h.modes
-        re = mode.freq
-        im = abs(mode.decay)
-        FREQ.append(re)
-        DECAY.append(im)
-unguided_dispersion = np.c_[FREQ, DECAY]
-np.save("unguided_dispersion.npy", unguided_dispersion)
+# def step(sim):
+#     sim: sim_td
+#     arr = sim.get_dft_array(dft_obj, mp.Ey, 0).copy()
+#     Iy.append(abs(arr) ** 2)
 
-# %% run for guided frequencies, index from low v to high v, truncating when
-#    the loss is sufficiently low
-FREQ = []
-DECAY = []
-for fcen, kx in k_points[unguided[-1] + 1 :]:
-    # ------ reset
-    sim.reset_meep()
 
-    k_point = mp.Vector3(kx, 0, 0)
+# sim_td.use_output_directory("eps/")
+# sim_td.run(
+#     mp.after_sources(mp.at_every(100, step)),
+#     until_after_sources=10000,  # 1 centimeter
+# )
 
-    # ------ sources
-    src = mp.GaussianSource(frequency=fcen, width=10)
-    # mp.NO_DIRECTION: eig_k_point is initial guess, search direction, and sets
-    # the direction of the waveguide
-    eigenmode = mp.EigenModeSource(
-        src,
-        center=mp.Vector3(),
-        # the source extends the cell, not including pml layers
-        size=mp.Vector3(
-            0,
-            TFW.cell_width - dpml * 2 - 1,
-            TFW.cell_height - dpml * 2 - 1,
-        ),
-        direction=mp.NO_DIRECTION,
-        eig_band=1,  # maybe it wasn't using the first band?
-        eig_kpoint=k_point,
-        eig_match_freq=True,
-    )
-    # sources = [mp.Source(src, mp.Ey, center=mp.Vector3())]  # point source
-    sources = [eigenmode]
-    sim.sources = sources
+# Iy = np.asarray(Iy)
 
-    # ------ set waveguide epsilon
-    eps_wvgd = eps_func_wvgd(fcen)
-    TFW.blk_wvgd.material = mp.Medium(epsilon=eps_wvgd)
-    TFW._blk_film.material = mp.Medium(epsilon=eps_wvgd)
 
-    # ------ set substrate epsilon
-    eps_sbstrt = TFW.sbstrt_mdm.epsilon(fcen)[2, 2]
-    TFW.blk_sbstrt.material = mp.Medium(epsilon=eps_sbstrt)
-    sim.geometry = TFW.geometry
+# %% ------ look at results ---------------------------------------------------
+# def video():
+#     fig, ax = plt.subplots(1, 1)
+#     for n, i in enumerate(Iy):
+#         ax.clear()
+#         ax.imshow(i.T[::-1])
+#         ax.set_title((n + 1) * 100)
+#         plt.pause(0.1)
 
-    # ------ harminv monitoring
-    df = 100e-3 / (1 / fcen) ** 2  # 100 nm
-    h = mp.Harminv(mp.Ey, mp.Vector3(0, 0, 0), fcen, df)
-
-    # ------ run simulation
-    sim.k_point = k_point
-    sim.run(mp.after_sources(h), until_after_sources=300)
-
-    # for propagation loss, I work close to the cut off frequency, there
-    # shouldn't be other modes close by
-    assert len(h.modes) == 1
-    (mode,) = h.modes
-    re = mode.freq
-    im = abs(mode.decay)
-    FREQ.append(re)
-    DECAY.append(im)
-
-    if im <= 1e-5:
-        break
-
-guided_dispersion = np.c_[FREQ, DECAY]
-np.save("guided_dispersion.npy", guided_dispersion)
+ps = 1e-12
+km = 1e3
+fig, ax = plt.subplots(1, 1)
+ax.plot(wl[:], beta2[:] / (ps**2 / km), "o")
+s = UnivariateSpline(wl[::-1], beta2[::-1] / (ps**2 / km), s=0)
+_ = np.linspace(wl.min(), wl.max(), 1000)
+ax.plot(_, s(_), linewidth=2)
+ax.set_xlabel("wavelength ($\\mathrm{\\mu m}$)")
+ax.set_ylabel("$\\mathrm{\\beta_2 \\; (ps^2/km})$")
+ax.set_title(str(np.round((sim.height - sim.etch_depth) * 1e3, 3)) + " nm slab")
+fig.tight_layout()
